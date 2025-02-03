00cc4126ff0e667a98f3952a67e84e74e54d0d24
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-02-03 19:27:21 +0300

    Folder changes

--- rooklang/flashcards/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-from .models import Flashcard
-
-def index(request):
-    flshcards = Flashcard.objects.all()
-    return render(request, 'flashcards/index.html', { 'flshcards' : flshcards})
