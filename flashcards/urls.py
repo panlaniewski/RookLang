61f8a4f83e1126d69d6eb4d93442423559a370fe
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-02-07 17:19:03 +0300

    Add creating categories

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index, by_category, FlashcardCreateView
+from .views import index, by_category, FlashcardCreateView, CategoryCreateView
 
 urlpatterns = [
-    path('add/', FlashcardCreateView.as_view(), name = 'add'),
+    path('add_category/', CategoryCreateView.as_view(), name = 'add_category'),
+    path('add_flashcard/', FlashcardCreateView.as_view(), name = 'add_flashcard'),
     path('<int:category_id>/', by_category, name = 'by_category'),
     path('', index, name = 'index'),
 ]
