e7dd1e8eff28d6d7081266bee486d9c0a081193c
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-03-08 23:41:27 +0300

    Frontend is over(?)

--- flashcards/static/flashcards/js/script.js
+++ flashcards/static/flashcards/js/script.js
@@ -1,9 +1,18 @@
-
+// -----------------------------------------------------------------------------------------------------
 const searchElement = document.querySelector('.search-header__wrapper');
 const searchButton = document.querySelector('.search-header__btn');
 const body = document.querySelector('body');
 
 searchButton.addEventListener('click', () => {
     searchElement.classList.toggle('search-header__btn_active');
-    body.classList.toggle('search_lock');
+    body.classList.toggle('_lock');
+});
+// -----------------------------------------------------------------------------------------------------
+const burgerButton = document.querySelector('.header__burger');
+const sideBar = document.querySelector('.page__sidebar');
+
+burgerButton.addEventListener('click', function () {
+   sideBar.classList.toggle('_sidebar-active');
+   this.classList.toggle('_burger-active');
+   body.classList.toggle('_lock');
 })
