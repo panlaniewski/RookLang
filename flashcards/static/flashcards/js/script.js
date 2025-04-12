b9d8d7134b5ac656d400f0260ffc614465e4d51c
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-04-12 20:03:50 +0300

    Learning flashcards is over

--- flashcards/static/flashcards/js/script.js
+++ flashcards/static/flashcards/js/script.js
@@ -20,8 +20,8 @@ burgerButton.addEventListener('click', function () {
 const getDataUrl = 'get_data/';
 const flashcardBlocks = document.querySelectorAll('.flashcard');
 const flashcardsContainer = document.querySelector('.flashcards__popup');
-const wordElement = document.getElementById("flashcard_word");
-const translateElement = document.getElementById("flashcard_translate");
+const wordElement = flashcardsContainer.querySelector('[data-flashcard="word"]');
+const translateElement = flashcardsContainer.querySelector('[data-flashcard="translate"]');
 let flashcardsCache = null;
 
 async function fetchFlashcards() {
@@ -51,12 +51,6 @@ flashcardBlocks.forEach(item => {
     });
 });
 
-window.document.addEventListener('click', event => {
-    if (!event.target.closest('.popup__wrapper')) {
-        flashcardsContainer.style.display = 'none';
-    }
-})
-
 function createFlashcard(data) {
     wordElement.innerHTML = data.word;
     translateElement.innerHTML = data.translate;
@@ -67,4 +61,64 @@ const card = document.querySelector('.popup__wrapper');
 
 card.addEventListener('click', () => {
     card.classList.toggle('is_flipped');
+})
+// ------------------------------------------------------------------------------------------------------------------
+const repeatButton = document.getElementById('btn-repeat');
+const learnContainer = document.querySelector('.page__learn');
+const learnWrapper = document.querySelector('.learn__wrapper');
+const learnWord = learnContainer.querySelector('[data-flashcard="word"]');
+const learnTranslate = learnContainer.querySelector('[data-flashcard="translate"]');
+const nextButton = document.querySelector('.learn__next');
+
+let shuffled_flashcards = [];
+let index = 0;
+
+repeatButton.addEventListener('click', async (event) => {
+    event.stopPropagation();
+
+    shuffled_flashcards = shuffle(await fetchFlashcards());
+    index = 0;
+
+    learnContainer.style.display = 'flex';
+    learnWrapper.style.display = 'flex';
+
+    showFleshcard(index);
+    nextButton.style.display = 'block'; 
+});
+
+function showFleshcard(index) {
+    if (index < shuffled_flashcards.length) {
+        const card = shuffled_flashcards[index];
+        learnWord.innerHTML = card.word;
+        learnTranslate.innerHTML = card.translate;
+    } else {
+        learnContainer.style.display = 'none'; 
+    }
+}
+
+nextButton.addEventListener('click', () => {
+    index++;
+    showFleshcard(index);
+});
+
+learnWrapper.addEventListener('click', () => {
+    learnWrapper.classList.toggle('is_flipped');
+    nextButton.style.display = 'block';
+})
+
+function shuffle(array) {
+    let result = array;
+    if (array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [result[i], result[j]] = [result[j], result[i]];
+        }
+    }
+    return result;
+}
+
+window.document.addEventListener('click', event => {
+    if (!event.target.closest('.popup__wrapper')) {
+        flashcardsContainer.style.display = 'none';
+    }
 })
